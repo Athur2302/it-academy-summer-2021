0a90e1a9250c8824f91a18d4aa7cd8c33c9a64f9
Arthur <71175378+Athur2302@users.noreply.github.com>
2021-06-28 18:50:47 +0300

    Update tesk8.py

--- src/2домашняя работа/tesk8.py
+++ src/2домашняя работа/tesk8.py
@@ -35,12 +35,10 @@ def split_and_join (line):
     a = line.split()
     lin = "-".join(a)
     return (lin)
-
 if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
-
 if __name__ == '__main__':
     a = int(input())
     b = int(input())

58a794966df1652a0c68b110b44ac20947cd4897
Arthur <71175378+Athur2302@users.noreply.github.com>
2021-06-28 20:01:12 +0300

    Update task7.py

--- src/2homework/task7.py
+++ src/2homework/task7.py
@@ -1,9 +1,8 @@
-# три стороны треугольника 
+# три стороны треугольника
 a = float(input("a:"))
 b = float(input("b:"))
 c = float(input("c:"))
-
-if a + b > c and a + c > b and b + c > a :
+if a + b > c and a + c > b and b + c > a:
     print("треугольник существует")
 else:
-    print ("не существует")
+    print("не существует")

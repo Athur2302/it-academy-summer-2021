3d775f12898a3bc546461b17ee4ca09e61eb5c72
Arthur <71175378+Athur2302@users.noreply.github.com>
2021-06-29 13:10:48 +0300

    Update task4.py

--- src/2homework/task4.py
+++ src/2homework/task4.py
@@ -4,9 +4,9 @@ b = 0
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 for i in x:
     if i in english:
-        if i.isupper() ==if cond:
+        if i.isupper() == if cond:
             a += 1
-        elif i.islower() ==if cond:
+        elif i.islower() == if cond:
             b += 1
 print(a, "больших прописных английских букв")
 print(b, "маленьких прописных английских букв")

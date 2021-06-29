81af93745c36c1499db47fb4552df20f80513966
Arthur <71175378+Athur2302@users.noreply.github.com>
2021-06-29 13:53:15 +0300

    Update tesk8.py

--- src/2домашняя работа/tesk8.py
+++ src/2домашняя работа/tesk8.py
@@ -16,7 +16,7 @@ for i in range(thickness + 1):
     print((c * thickness).center(thickness * 2) + (c * thickness).center(thickness * 6))
 
 for i in range(thickness):
-    print(((c * (thickness - i - 1)) .rjust(thickness) + c + \r
+    print(((c * (thickness - i - 1)) .rjust(thickness) + c + \
            (c * (thickness - i - 1)).ljust(thickness)).rjust(thickness * 6))
 
 # 2 задание

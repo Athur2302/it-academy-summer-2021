0bdd140ce0f36e09234718f884843beb42461a8c
Arthur <71175378+Athur2302@users.noreply.github.com>
2021-06-29 11:06:51 +0300

    Update task4.py

--- src/2homework/task4.py
+++ src/2homework/task4.py
@@ -1,12 +1,12 @@
-x= list(input("Введите строку: "))
+x= list (input ("Введите строку: "))
 a = 0
 b = 0
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 for i in x:
     if i in english:
-        if i.isupper() == True:
+        if i.isupper() == if cond is True:
             a += 1
-        elif i.islower() == True:
+        elif i.islower() == if cond is True::
             b += 1
-print(a ," больших прописных английских букв")
-print(b ,"маленьких прописных английских букв")
+print( a," больших прописных английских букв")
+print( b,"маленьких прописных английских букв")
